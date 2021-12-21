819bba70e82608ec3b61e306feac7c6c52b49717
tom_glb <goodMoring_glb@atguigu.com>
2021-12-21 10:04:59 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -61,6 +61,14 @@ module.exports = {
         ],
       },
       {
+        text: 'AI',
+        link: '/ai/',
+        items: [
+          { text: 'Tensorflow', link: '/pages/862ae0/' },
+          { text: 'Pytorch', link: '/pages/24af9b/' },
+        ],
+      },
+      {
         text: '技术',
         link: '/technology/',
         items: [
@@ -81,7 +89,6 @@ module.exports = {
           { text: '友情链接', link: '/friends/' },
         ],
       },
-      { text: '关于', link: '/about/' },
       {
         text: '收藏',
         link: '/pages/beb6c0bd8a66cea6/',
@@ -227,17 +234,8 @@ module.exports = {
         thirdparty: [
           // 可选，默认 []
           {
-            title: '在MDN中搜索',
-            frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-            behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
-          },
-          {
-            title: '在Runoob中搜索',
-            frontUrl: 'https://www.runoob.com/?s=',
-          },
-          {
-            title: '在Vue API中搜索',
-            frontUrl: 'https://cn.vuejs.org/v2/api/#',
+            title: '在Google中搜索',
+            frontUrl: 'https://www.google.com.hk/search?q=',
           },
           {
             title: '在Bing中搜索',
