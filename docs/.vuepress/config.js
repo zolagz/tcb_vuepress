4a9452b274dddb7197aae08556ef0fc079edcf01
HK <le_pencil@126.com>
2022-07-05 09:59:32 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -56,20 +56,8 @@ module.exports = {
         text: '后端',
         link: '/back/',
         items: [
-          { text: 'Java', link: '/pages/8309a5b876fc95e3/' },
-          { text: 'Python', link: '/pages/ccbcd8/' },
-          { text: 'Go语言', link: '/pages/4abe03/' },
-          { text: 'Linux', link: '/pages/207ce4/' },
-
-        ],
-      },
-      {
-        text: 'AI',
-        link: '/ai/',
-        items: [
-          { text: 'Tensorflow', link: '/pages/862ae0/' },
-          { text: 'Pytorch', link: '/pages/24af9b/' },
-          { text: '计算机视觉', link: '/pages/2b3d8b/' },
+          { text: 'JAVA', link: '/pages/8309a5b876fc95e3/' },
+          { text: 'PYTHON', link: '/pages/0a83b083bdf257cb/' },
         ],
       },
       {
@@ -93,6 +81,7 @@ module.exports = {
           { text: '友情链接', link: '/friends/' },
         ],
       },
+      { text: '关于', link: '/about/' },
       {
         text: '收藏',
         link: '/pages/beb6c0bd8a66cea6/',
@@ -229,6 +218,7 @@ module.exports = {
     //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
     // }],
 
+
     'vuepress-plugin-baidu-autopush', // 百度自动推送
 
     // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
@@ -238,8 +228,17 @@ module.exports = {
         thirdparty: [
           // 可选，默认 []
           {
-            title: '在Google中搜索',
-            frontUrl: 'https://www.google.com.hk/search?q=',
+            title: '在MDN中搜索',
+            frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+            behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+          },
+          {
+            title: '在Runoob中搜索',
+            frontUrl: 'https://www.runoob.com/?s=',
+          },
+          {
+            title: '在Vue API中搜索',
+            frontUrl: 'https://cn.vuejs.org/v2/api/#',
           },
           {
             title: '在Bing中搜索',
@@ -262,6 +261,32 @@ module.exports = {
         showInMobile: false, // whether to display on the mobile side, default: false.
       },
     ],
+     ['vuepress-plugin-readmore-popular', {
+      // 已申请的博客 ID
+      blogId: '18762-1609305354821-257',
+      // 已申请的微信公众号名称
+      name: '全栈技术驿站',
+      // 已申请的微信公众号回复关键词
+      keyword: 'Tech',                    
+      // 已申请的微信公众号二维码链接
+      qrcode: 'https://www.techgrow.cn/img/wx_mp_qr.png',
+      // 文章内容的 JS 选择器，若使用的不是官方默认主题，则需要根据第三方的主题来设置
+      selector: '.theme-vdoing-content',
+      // 自定义的 JS 资源链接，可用于 CDN 加速
+      libUrl: 'https://qiniu.techgrow.cn/readmore/dist/readmore.js',
+      // 自定义的 CSS 资源链接，可用于适配不同风格的博客
+      cssUrl: 'https://qiniu.techgrow.cn/readmore/dist/vuepress.css',
+      // 文章排除添加引流工具的 URL 规则，支持使用路径、通配符、正则表达式的匹配规则
+      excludes: { strExp: [], regExp: [] },
+      // 是否反转 URL 排除规则的配置，即只有符合排除规则的文章才会添加引流工具
+      reverse: false,
+      // 文章解锁后凭证的有效天数
+      expires: 365,
+      // 定时校验凭证有效性的时间间隔（秒）
+      interval: 60,
+      // 每篇文章随机添加引流工具的概率，有效范围在 0.1 ~ 1 之间，1 则表示所有文章默认都自动添加引流工具
+      random: 1
+    }],
 
     [
       'demo-block', // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
